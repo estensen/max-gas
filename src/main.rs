7d814ef1c0b18862e3a90ec4c41eb6d3b27dae73
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-09-08 15:08:56 +0200

    Extract handle block to func

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use alloy::providers::{Provider, ProviderBuilder};
+use alloy::{
+    eips::BlockNumberOrTag,
+    providers::{Provider, ProviderBuilder},
+};
+use alloy_rpc_types_eth::Block;
 use dotenv::dotenv;
 use eyre::Result;
 use std::env;
 
 fn get_api_key() -> String {
     // Load .env file if present
+    // https://eth.merkle.io is free
     dotenv().ok();
     env::var("RPC_URL").expect("RPC_URL must be set")
+}
+
+fn handle_block(block: Block) {
+    let latest_block_number = block.header.number;
+    println!("Latest block number: {latest_block_number}");
 }
 
 #[tokio::main]
@@ -14,8 +24,14 @@ async fn main() -> Result<()> {
     let rpc_url = get_api_key().parse()?;
     let provider = ProviderBuilder::new().on_http(rpc_url);
 
-    let latest_block_number = provider.get_block_number().await?;
-    println!("Latest block number: {latest_block_number}");
+    let latest_block = provider
+        .get_block_by_number(BlockNumberOrTag::Latest, true)
+        .await?;
+    if let Some(block) = latest_block {
+        handle_block(block);
+    } else {
+        println!("Unable to get latest block");
+    }
 
     Ok(())
 }
