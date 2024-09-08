c6ff5e0e1a78277acfbe894b45f7f09cb68be8dd
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-09-08 14:35:26 +0200

    Connect to alchemy with websocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use dotenv::dotenv;
+use std::env;
+
+fn get_api_key() -> String {
+    // Load .env file if present
+    dotenv().ok();
+    env::var("RPC_URL").expect("RPC_URL must be set")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rpc_url = get_api_key();
+    println!("RPC URL: {}", rpc_url);
 }
